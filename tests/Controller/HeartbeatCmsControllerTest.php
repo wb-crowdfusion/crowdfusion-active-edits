e2fa7da0f396c357fa6b23b4d5cf8502607d76b0
Michael H. Arieli <excelwebzone@gmail.com>
2015-06-01 11:15:21 -0700

    RE #1: Implemented 2 test examples

--- tests/Controller/HeartbeatCmsControllerTest.php
+++ tests/Controller/HeartbeatCmsControllerTest.php
@@ -10,17 +10,34 @@ class HeartbeatCmsControllerTest extends \PHPUnit_Framework_TestCase
     /** @var HeartbeatCmsController */
     protected $controller;
 
+    /** @var \MemcachedCacheStore */
+    protected $cache;
+
+    /** @var \Request */
+    protected $request;
+
+    /** @var \stdClass */
+    protected $user;
+
     protected function setUp()
     {
-        $Logger             = $this->getMock('LoggerInterface');
+        $Logger = $this->getMock('LoggerInterface');
 
-        $Request = $this
-            ->getMockBuilder('Request')
-            ->disableOriginalConstructor()
+        $InputClean = $this
+            ->getMockBuilder('InputCleanInterface')
+            ->setConstructorArgs(array('/tmp', 'utf-8'))
             ->getMock()
         ;
+        $Request       = new \Request($InputClean, '/');
+        $this->request = $Request;
 
-        $RequestContext     = $this->getMock('RequestContext');
+        $this->user        = new \stdClass();
+        $this->user->Slug  = 'jonnytest';
+        $this->user->Title = 'Jonny Test';
+
+        $RequestContext = new \RequestContext();
+        $RequestContext->setUser($this->user);
+
         $TransactionManager = $this->getMock('TransactionManagerInterface');
 
         $Nonces = $this
@@ -29,14 +46,14 @@ class HeartbeatCmsControllerTest extends \PHPUnit_Framework_TestCase
             ->getMock()
         ;
 
-        $Permissions        = $this->getMock('Permissions');
+        $Permissions = $this->getMock('Permissions');
         $DateFactory = $this
             ->getMockBuilder('DateFactory')
             ->disableOriginalConstructor()
             ->getMock()
         ;
 
-        $ModelMapper        = $this->getMock('ModelMapper');
+        $ModelMapper = $this->getMock('ModelMapper');
 
         $Session = $this
             ->getMockBuilder('Session')
@@ -68,9 +85,12 @@ class HeartbeatCmsControllerTest extends \PHPUnit_Framework_TestCase
             MemcachedMock::create();
         }
 
-        $MemcachedCacheStore = new \MemcachedCacheStore(
+        $this->cache = new \MemcachedCacheStore(
             new \NullLogger(),
-            [['host' => '127.0.0.1', 'port' => 11211]],
+            array(array(
+                'host' => '127.0.0.1',
+                'port' => 11211
+            )),
             sprintf('test_%s_', md5(rand())),
             true,
             'cf_memcached_tests',
@@ -80,7 +100,7 @@ class HeartbeatCmsControllerTest extends \PHPUnit_Framework_TestCase
             false
         );
 
-        $this->controller->setPrimaryCacheStore($MemcachedCacheStore);
+        $this->controller->setPrimaryCacheStore($this->cache);
     }
 
     protected function tearDown()
@@ -88,112 +108,80 @@ class HeartbeatCmsControllerTest extends \PHPUnit_Framework_TestCase
         unset($this->controller);
     }
 
-    public function testCreateController()
+    public function testCreate()
     {
         $this->assertInstanceOf('\AbstractCmsController', $this->controller);
     }
 
-/*
-    public function testTotalMembersAction()
+    /**
+     * @dataProvider getSlugs
+     */
+    public function testGetMembersAction($slug)
     {
-        $results = $this->controller->totalMembersAction();
+        $this->request->addRouteParameters(array('slug' => $slug));
 
-        $this->assertNull($results);
+        ob_start();
+        $this->controller->getMembersAction();
+        ob_end_flush();
+
+        $output = ob_get_contents();
+
+        $this->assertJsonStringEqualsJsonString($output, json_encode(array(array(
+            'slug'       => $this->user->Slug,
+            'name'       => $this->user->Title,
+            'activeDate' => null,
+            'updateMeta' => false
+        ))));
     }
 
-    public function testTotalMembersActionSlugDoesNotExist()
+    /**
+     * @dataProvider getSlugs
+     */
+    public function testTotalMembersAction($slug)
     {
-        return $this->controller->totalMembersAction();
+        $this->request->addRouteParameters(array('slugs' => $slug));
+
+        ob_start();
+        $this->controller->totalMembersAction();
+        ob_end_flush();
+
+        $output = ob_get_contents();
+
+        $this->assertJsonStringEqualsJsonString($output, json_encode(array(
+            $slug => array(array(
+                'slug' => $this->user->Slug,
+                'name' => $this->user->Title,
+            ))
+        )));
     }
 
-    public function testTotalMembersActionZeroSlug()
+    public function getSlugs()
     {
-        return $this->controller->totalMembersAction();
+        $slugs = array();
+
+        for ($i=0; $i<10; $i++) {
+            $slugs[] = array(sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+        		// 32 bits for "time_low"
+        		mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+        		// 16 bits for "time_mid"
+        		mt_rand(0, 0xffff),
+
+        		// 16 bits for "time_hi_and_version",
+        		// four most significant bits holds version number 4
+        		mt_rand(0, 0x0fff) | 0x4000,
+
+        		// 16 bits, 8 bits for "clk_seq_hi_res",
+        		// 8 bits for "clk_seq_low",
+        		// two most significant bits holds zero and one for variant DCE1.1
+        		mt_rand(0, 0x3fff) | 0x8000,
+
+        		// 48 bits for "node"
+        		mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+    		));
+        }
+
+        return $slugs;
     }
-
-    public function testTotalMembersActionOneSlug()
-    {
-        return $this->controller->totalMembersAction();
-    }
-
-    public function testTotalMembersActionTwoSlug()
-    {
-        return $this->controller->totalMembersAction();
-    }
-
-    public function testTotalMembersActionTwentySlug()
-    {
-        return $this->controller->totalMembersAction();
-    }
-
-    public function testGetMembersAction()
-    {
-        return $this->controller->getMembersAction();
-    }
-
-    public function testGetMembersActionSlugDoesNotExist()
-    {
-        return $this->controller->getMembersAction();
-    }
-
-    public function testGetMembersActionUserExist()
-    {
-        return $this->controller->getMembersAction();
-    }
-
-    public function testGetMembersActionUserDoesNotExist()
-    {
-        return $this->controller->getMembersAction();
-    }
-
-    public function testRemoveMemberAction()
-    {
-        return $this->controller->removeMemberAction();
-    }
-
-    public function testRemoveMemberActionSlugExistUserDoesExist()
-    {
-
-    }
-
-    public function testRemoveMemberActionSlugExistUserDoesNotExist()
-    {
-
-    }
-
-    public function testRemoveMemberActionSlugDoesNotExist()
-    {
-
-    }
-
-    public function testUpdateMetaAction()
-    {
-        return $this->controller->updateMetaAction();
-    }
-
-    public function testUpdateMetaActionSlugDoesNotExist()
-    {
-
-    }
-
-    public function testUpdateMetaActionSlugDoesExistUserExist()
-    {
-
-    }
-
-    public function testUpdateMetaActionSlugDoesExistUserExistMetaIsTrue()
-    {
-
-    }
-
-    public function testUpdateMetaActionSlugDoesExistUserExistMetaIsFalse()
-    {
-
-    }
-
-    public function testUpdateMetaActionSlugDoesExistUserDoesNotExist()
-    {
-
-    }
-*/
 }
