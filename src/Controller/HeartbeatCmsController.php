c1fbfc44813f8067e0f5e8d741cfcfb4e02fb5a1
Michael H. Arieli <excelwebzone@gmail.com>
2015-06-29 13:09:18 -0700

    RE #1: Fixed microseconds to minutes conversion

--- src/Controller/HeartbeatCmsController.php
+++ src/Controller/HeartbeatCmsController.php
@@ -225,7 +225,7 @@ class HeartbeatCmsController extends \AbstractCmsController
             if ($lock = $this->PrimaryCacheStore->get($this->getLockKey($slug))) {
                 usleep($i += 200000); // 200 milliseconds
             }
-        } while ($lock && $i < 3000);
+        } while ($lock && $i < 180000000); // 3 minutes
 
         // failed after 3 seconds
         if ($lock) {
